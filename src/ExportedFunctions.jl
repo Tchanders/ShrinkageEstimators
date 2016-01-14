625623527327f302cc17dca059b66279d87e152e
Thalia Chan <thalia.e.chan@googlemail.com>
2016-01-14 15:09:04 +0000

    Rename exported functions and add documentation

--- src/ExportedFunctions.jl
+++ src/ExportedFunctions.jl
@@ -1,31 +1,100 @@
 # Exported functions
 
-export getentropy, getmutualinformation, getconditionalmutualinformation
+# TODO:
+# Offer alternative target
+# Look into standardising the documentation
+# Offer alternative discretisation methods
 
-function getentropy(values::Array{Float64,2}, base=2, lambda=false)
-	probabilities = getprobabilitiesshrinkage(getfrequencies(values), lambda)
+export get_shrinkage_entropy, get_shrinkage_mi, get_shrinkage_cmi
+
+
+"""
+Calculates a James-Stein shrinkage estimate for the entropy of a set
+of observed values, via the following steps:
+(1) convert the observed values to frequencies of discrete bins
+(2) convert the frequencies to probablities using shrinkage estimation
+(3) run the probabilities through the entropy formula.
+Can calculate joint entropy for up to 3 sets of observed values.
+
+Parameters:
+
+valuesX - dxn Array{Float64,2} - The observed values, where d is
+the number of dimensions and n is the number of values.
+
+[valuesY] - dxn Array{Float64,2} - The observed values, where d is
+the number of dimensions and n is the number of values.
+
+[valuesZ] - dxn Array{Float64,2} - The observed values, where d is
+the number of dimensions and n is the number of values.
+
+[base=2] - Int - The base of the logarithm (this determines the units).
+
+[lambda=false] - Int - The shrinkage intensity, between 0 and 1
+inclusive. If omitted, the optimal lambda it will be calculated.
+"""
+function get_shrinkage_entropy(valuesX::Array{Float64,2}, base=2, lambda=false)
+	probabilities = getprobabilitiesshrinkage(getfrequencies(valuesX), lambda)
 	return applyentropyformula(probabilities, base)
 end
-function getentropy(valuesX::Array{Float64,2}, valuesY::Array{Float64,2}, base=2, lambda=false)
+function get_shrinkage_entropy(valuesX::Array{Float64,2}, valuesY::Array{Float64,2}, base=2, lambda=false)
 	jointprobabilities = getprobabilitiesshrinkage(getjointfrequencies(valuesX, valuesY), lambda)
 	return applyentropyformula(jointprobabilities, base)
 end
-function getentropy(valuesX::Array{Float64,2}, valuesY::Array{Float64,2}, valuesZ::Array{Float64,2}, base=2, lambda=false)
+function get_shrinkage_entropy(valuesX::Array{Float64,2}, valuesY::Array{Float64,2}, valuesZ::Array{Float64,2}, base=2, lambda=false)
 	jointprobabilities = getprobabilitiesshrinkage(getjointfrequencies(valuesX, valuesY, valuesZ), lambda)
 	return applyentropyformula(jointprobabilities, base)
 end
 
-function getmutualinformation(valuesX::Array{Float64,2}, valuesY::Array{Float64,2}, base=2, lambda=false)
-	entropyX = getentropy(valuesX, base, lambda)
-	entropyY = getentropy(valuesY, base, lambda)
-	entropyXY = getentropy(valuesX, valuesY, base, lambda)
+"""
+Calculates a James-Stein shrinkage estimate for the mutual information
+between two sets of observed values.
+
+Parameters:
+
+valuesX - dxn Array{Float64,2} - A set of observed values, where d is
+the number of dimensions and n is the number of values.
+
+valuesY - dxn Array{Float64,2} - Another set of observed values, where
+d is the number of dimensions and n is the number of values.
+
+[base=2] - Int - The base of the logarithm (this determines the units).
+
+[lambda=false] - Int - The shrinkage intensity, between 0 and 1
+inclusive. If omitted, the optimal lambda it will be calculated.
+"""
+function get_shrinkage_mi(valuesX::Array{Float64,2}, valuesY::Array{Float64,2}, base=2, lambda=false)
+	entropyX = get_shrinkage_entropy(valuesX, base, lambda)
+	entropyY = get_shrinkage_entropy(valuesY, base, lambda)
+	entropyXY = get_shrinkage_entropy(valuesX, valuesY, base, lambda)
 	return applymutualinformationformula(entropyX, entropyY, entropyXY)
 end
 
-function getconditionalmutualinformation(valuesX::Array{Float64,2}, valuesY::Array{Float64,2}, valuesZ::Array{Float64,2}, base=2, lambda=false)
-	entropyZ = getentropy(valuesZ, base, lambda)
-	entropyXZ = getentropy(valuesX, valuesZ, base, lambda)
-	entropyYZ = getentropy(valuesY, valuesZ, base, lambda)
-	entropyXYZ = getentropy(valuesX, valuesY, valuesZ, base, lambda)
+"""
+Calculates a James-Stein shrinkage estimate for the conditional mutual
+information between two sets of observed values, given a third set of
+observed values.
+
+Parameters:
+
+valuesX - dxn Array{Float64,2} - A set of observed values, where d is
+the number of dimensions and n is the number of values.
+
+valuesY - dxn Array{Float64,2} - Another set of observed values, where
+d is the number of dimensions and n is the number of values.
+
+valuesZ - dxn Array{Float64,2} - The set of observed values, that will
+be conditioned on, where d is the number of dimensions and n is the
+number of values.
+
+[base=2] - Int - The base of the logarithm (this determines the units).
+
+[lambda=false] - Int - The shrinkage intensity, between 0 and 1
+inclusive. If omitted, the optimal lambda it will be calculated.
+"""
+function get_shrinkage_cmi(valuesX::Array{Float64,2}, valuesY::Array{Float64,2}, valuesZ::Array{Float64,2}, base=2, lambda=false)
+	entropyZ = get_shrinkage_entropy(valuesZ, base, lambda)
+	entropyXZ = get_shrinkage_entropy(valuesX, valuesZ, base, lambda)
+	entropyYZ = get_shrinkage_entropy(valuesY, valuesZ, base, lambda)
+	entropyXYZ = get_shrinkage_entropy(valuesX, valuesY, valuesZ, base, lambda)
 	return applyconditionalmutualinformationformula(entropyZ, entropyXZ, entropyYZ, entropyXYZ)
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -19,31 +19,31 @@ arrZ = rand(1, 10000)
 # is going drastically wrong.
 
 # Check entropy is roughly log_base(b) for uniform distribution with b bins
-@test_approx_eq_eps getentropy(arr) log2(b) 0.5
-println(getentropy(arr) - log2(b))
+@test_approx_eq_eps get_shrinkage_entropy(arr) log2(b) 0.5
+println(get_shrinkage_entropy(arr) - log2(b))
 println("Entropy passed")
 
 # Check change of base works
-@test_approx_eq_eps getentropy(arr, e) log(b) 0.5
+@test_approx_eq_eps get_shrinkage_entropy(arr, e) log(b) 0.5
 println("Entropy with change of base passed")
 
 # Check specifying lambda works
-@test_approx_eq_eps getentropy(arr, 2, 0) log2(b) 0.5
+@test_approx_eq_eps get_shrinkage_entropy(arr, 2, 0) log2(b) 0.5
 println("Entropy with with lambda 0 passed")
 
 # Check joint entropy between multiple identical distributions is roughly
 # the same as entropy for one of them
-@test_approx_eq_eps getentropy(arr) getentropy(arr, arr) 0.5
+@test_approx_eq_eps get_shrinkage_entropy(arr) get_shrinkage_entropy(arr, arr) 0.5
 println("Joint entropy between 2 variables passed")
-@test_approx_eq_eps getentropy(arr) getentropy(arr, arr, arr) 0.5
+@test_approx_eq_eps get_shrinkage_entropy(arr) get_shrinkage_entropy(arr, arr, arr) 0.5
 println("Joint entropy between 3 variables passed")
 
 # Check I(X; Y) is H(X) + H(Y) - H(X, Y)
-a = getentropy(arrX) + getentropy(arrY) - getentropy(arrX, arrY)
-@test a == getmutualinformation(arrX, arrY)
+a = get_shrinkage_entropy(arrX) + get_shrinkage_entropy(arrY) - get_shrinkage_entropy(arrX, arrY)
+@test a == get_shrinkage_mi(arrX, arrY)
 println("Mutual information passed")
 
 # Check I(X; Y | Z) is H(X, Z) + H(Y, Z) - H(X, Y, Z) - H(Z)
-a = getentropy(arrX, arrZ) + getentropy(arrY, arrZ) - getentropy(arrX, arrY, arrZ) - getentropy(arrZ)
-@test a == getconditionalmutualinformation(arrX, arrY, arrZ)
+a = get_shrinkage_entropy(arrX, arrZ) + get_shrinkage_entropy(arrY, arrZ) - get_shrinkage_entropy(arrX, arrY, arrZ) - get_shrinkage_entropy(arrZ)
+@test a == get_shrinkage_cmi(arrX, arrY, arrZ)
 println("Conditional mutual information passed")
